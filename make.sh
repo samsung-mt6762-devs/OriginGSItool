4d27194b2cb03be2b2328b375fbb1e5a8398dcb0
Yordan A <106074709+Dachoni03@users.noreply.github.com>
2023-08-21 00:45:36 +0300

    Removed treble check

--- make.sh
+++ make.sh
@@ -117,11 +117,6 @@ else
     echo "ro.build.system_root_image=true" >> "$systemdir/system/build.prop"
 fi
 
-# Detect is the src treble ro.treble.enabled=true
-istreble=`cat $systemdir/system/build.prop | grep ro.treble.enabled | cut -d "=" -f 2`
-if [[ ! "$istreble" == "true" ]]; then
-    echo "The source is not treble supported"
-    exit 1
 fi
 
 # Detect Source API level
